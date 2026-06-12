f3638da581486903d526eb3d87e39ef3b7168719
StackBase <243320022+stack-base@users.noreply.github.com>
2026-06-12 21:15:31 +0530

    Update

--- orion/orion.js
+++ orion/orion.js
@@ -154,7 +154,7 @@ const showLoginModal=()=>{
             // SECURITY NOTIFICATION: If the user is an admin, trigger the webhook
             if (ADMIN_UIDS.includes(userCredential.user.uid)) {
                 // Replace this URL with your actual deployment URL later
-                const webhookUrl = 'https://script.google.com/macros/s/AKfycbypeVOZjYMTVx_kEk3lx2lkdF--B7IkYYRA15mGH-rwBFB28ZzC4fnoMNEIruZmMddfXA/exec'; 
+                const webhookUrl = 'https://script.google.com/macros/s/AKfycbyjSiClQVj8Pmq1kW5HghZ5cQ9z_VWSYsFSL8mh9A56KEUKNDmD25nUJpo37XUxzXcgRQ/exec'; 
                 
                 // Fire and forget the request so it doesn't slow down the UI
                 fetch(webhookUrl, {
